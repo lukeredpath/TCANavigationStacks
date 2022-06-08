a9841177f05a9f9317daa22e721e289f2356f278
Luke Redpath <luke@lukeredpath.co.uk>
2022-06-08 16:58:55 +0100

    Exploring buttons.

--- TCANavigationStacks/ContentView.swift
+++ TCANavigationStacks/ContentView.swift
@@ -11,10 +11,12 @@ import SwiftUI
 struct AppState: Equatable {
     var items: IdentifiedArrayOf<Item>
     var itemDetail: ItemDetailState?
+
     @BindableState var path: [Route] = []
 
     enum Route: Equatable, Hashable {
-        case itemDetail(id: Item.ID)
+        case itemDetail(Item.ID)
+        case itemDetailRoute(ItemDetailState.Route)
     }
 }
 
@@ -30,20 +32,23 @@ extension AppState {
 
 enum AppAction: BindableAction {
     case binding(BindingAction<AppState>)
+    case tappedItem(Item.ID)
     case itemDetail(ItemDetailAction)
 }
 
 let appReducer = Reducer<AppState, AppAction, Void> { state, action, _ in
     switch action {
+    case .itemDetail(.tappedChildButton):
+        state.path.append(.itemDetailRoute(.child))
+        return .none
+    case let .tappedItem(id):
+        guard let item = state.items[id: id] else { return .none }
+        state.itemDetail = .init(item: item)
+        state.path.append(.itemDetail(item.id))
+        return .none
     case .binding(\.$path):
-        guard let route = state.path.last else {
+        if state.path.isEmpty {
             state.itemDetail = nil
-            return .none
-        }
-        switch route {
-        case let .itemDetail(id):
-            guard let item = state.items[id: id] else { return .none }
-            state.itemDetail = .init(item: item)
         }
         return .none
     case .binding:
@@ -54,17 +59,21 @@ let appReducer = Reducer<AppState, AppAction, Void> { state, action, _ in
 }
 .binding()
 
-struct Item: Identifiable, Equatable {
+struct Item: Identifiable, Equatable, Hashable {
     var id: UUID = .init()
     var name: String
 }
 
 struct ItemDetailState: Equatable {
     let item: Item
+
+    enum Route {
+        case child
+    }
 }
 
 enum ItemDetailAction {
-    case stub
+    case tappedChildButton
 }
 
 struct ContentView: View {
@@ -75,8 +84,15 @@ struct ContentView: View {
             NavigationStack(path: viewStore.binding(\.$path)) {
                 List {
                     ForEach(viewStore.items) { item in
-                        NavigationLink(item.name, value: AppState.Route.itemDetail(id: item.id))
+//                        NavigationLink(item.name, value: AppState.Route.itemDetail(id: item.id))
+                        Button {
+                            viewStore.send(.tappedItem(item.id))
+                        } label: {
+                            Text(item.name)
+                        }
+                        .buttonStyle(NavigationLinkButtonStyle())
                     }
+                    NavigationLink("Navigation Link", value: "foo")
                 }
                 .navigationTitle("Items")
                 .navigationDestination(for: AppState.Route.self) { route in
@@ -89,10 +105,25 @@ struct ContentView: View {
                             ),
                             then: DetailView.init(store:)
                         )
+                    case .itemDetailRoute(.child):
+                        Text("Child View")
                     }
                 }
             }
         }
+    }
+}
+
+struct NavigationLinkButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.label
+            Spacer()
+            Image(systemName: "chevron.right")
+                .font(.footnote.weight(.bold))
+                .foregroundColor(Color(UIColor.tertiaryLabel))
+        }
+        .contentShape(Rectangle())
     }
 }
 
@@ -102,7 +133,17 @@ struct DetailView: View {
     var body: some View {
         WithViewStore(store) { viewStore in
             Text(viewStore.item.name)
+
+            Button("Open Child") {
+                viewStore.send(.tappedChildButton)
+            }
         }
+//        .navigationDestination(for: ItemDetailState.Route.self) { route in
+//            switch route {
+//            case .child:
+//                Text("Child View")
+//            }
+//        }
     }
 }
 
